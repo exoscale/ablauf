84e0e637749e93db4e59d6ac1ab63d5e439b9df8
mcorbin <corbin.math@gmail.com>
2020-09-24 10:49:44 +0200

    Version 0.1.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ablauf "0.1.7"
+(defproject exoscale/ablauf "0.1.8-SNAPSHOT"
   :description "long-running workflow management"
   :url "https://github.com/exoscale/ablauf"
   :license {:name "ISC License"
