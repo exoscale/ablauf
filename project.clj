81ef21d2329897279c96cb82949a7df6997e33e0
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-12-02 18:10:58 +0100

    Version 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ablauf "0.1.12-SNAPSHOT"
+(defproject exoscale/ablauf "0.1.12"
   :description "long-running workflow management"
   :url "https://github.com/exoscale/ablauf"
   :license {:name "ISC License"
