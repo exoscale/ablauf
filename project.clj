adf47140126df69a24bb5c9f17be32d741bd005d
Miguel Ping <miguel.ping@exoscale.ch>
2023-11-03 12:46:07 +0000

    Version 0.3.12-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/ablauf "0.3.11"
+(defproject exoscale/ablauf "0.3.12-SNAPSHOT"
   :description "long-running workflow management"
   :url "https://github.com/exoscale/ablauf"
   :license {:name "ISC License"
